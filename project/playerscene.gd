8dcba693578221121e3be4c0f4df242cc1f78b09
carterbuell <95313575+carterbuell@users.noreply.github.com>
2021-11-30 19:24:37 -0500

    added movemnt

--- project/playerscene.gd
+++ project/playerscene.gd
@@ -7,8 +7,16 @@ func _physics_process(delta):
 	var velocity = Vector2(0, 0)
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = 100
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -100
 	else:
 		velocity.x = 0
+	
+	#if !is_on_floor():
+	#	velocity.y += 100
+	#else: velocity = 0.1
+	velocity.y += 100
+	move_and_slide(velocity)
 
 func _process(delta):
 	pass
