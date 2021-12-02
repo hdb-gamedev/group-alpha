b71df38adf2496756ba4b8c77a2f4fbf5c611c5b
carterbuell <95313575+carterbuell@users.noreply.github.com>
2021-12-02 09:21:02 -0500

    CZC

--- project/playerscene.gd
+++ project/playerscene.gd
@@ -7,9 +7,9 @@ func _ready():
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = 100
+		velocity.x = 150
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -100
+		velocity.x = -150
 	else:
 		velocity.x = 0
 	
@@ -19,6 +19,9 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_up") && is_on_floor():
 		velocity.y = -300
+	
+	if is_on_ceiling():
+		velocity.y = 10
 
 	move_and_slide(velocity, Vector2(0, -1))
 
