b6a9cc376b7aa4243fd5d477688afc143c2a15cb
carterbuell <95313575+carterbuell@users.noreply.github.com>
2021-12-02 08:37:19 -0500

    cvbn

--- project/playerscene.gd
+++ project/playerscene.gd
@@ -1,26 +1,21 @@
 extends KinematicBody2D
-var velocity = Vector2(0, 0)
 
 func _ready():
 	pass 
 
 func _physics_process(delta):
-	#leftright movemnt
+	var velocity = Vector2(0, 0)
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = 100
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -100
 	else:
 		velocity.x = 0
-		
-	#jump
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.y = -300
 	
-	#if !is_on_floor():
-	#	velocity.y += 100
-	#else: velocity = 0.1
-	velocity.y += 10
+	if !is_on_floor():
+		velocity.y += 100
+	else: velocity = 0.1
+	velocity.y += 100
 	move_and_slide(velocity)
 
 func _process(delta):
