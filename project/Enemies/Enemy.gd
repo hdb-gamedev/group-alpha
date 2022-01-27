65acea71efc10bfedbe930c9d46073922b0c47af
carterbuell <95313575+carterbuell@users.noreply.github.com>
2022-01-27 15:56:34 -0500

    added animated sprite to enemy

--- project/Enemies/Enemy.gd
+++ project/Enemies/Enemy.gd
@@ -1,16 +1,24 @@
 extends KinematicBody2D
 var velocity = Vector2(0, 0)
+const Player = preload("res://Player/playerscene.gd")
 
 func _ready():
 	resetVelocity()
+	$AnimationPlayer.play("walk")
 	
 func _physics_process(delta):
 	if(!is_on_floor()):
 		velocity.y += 15
 	elif is_on_floor():
 		velocity.y = 0.1
-	if(velocity.x < 1):
-		resetVelocity()
+	
+	if(is_on_wall()):
+		velocity.x *= -1
+	
+	if velocity.x > 0:
+		$Sprite.flip_h = true
+	elif velocity.x < 0:
+		$Sprite.flip_h = false
 		
 	move_and_slide(velocity, Vector2(0, -1))
 	
@@ -21,3 +29,9 @@ func resetVelocity():
 	else:
 		velocity.x -= 50
 		
+func _on_Area2D_body_entered(body):
+	if body is Player:
+		body.die()
+		
+func enemyDie():
+	position = Vector2(rand_range(-1750, 1850), -600)
--- project/Levels/respawn.gd
+++ project/Levels/respawn.gd
@@ -1,7 +1,9 @@
 extends Area2D
 const Player = preload("res://Player/playerscene.gd")
+const Enemy = preload("res://Enemies/Enemy.gd")
 
 func _on_respawn_body_entered(body):
 	if body is Player:
 		body.die()
-		
+	if body is Enemy:
+		body.enemyDie()
